3703ba818b58ee94dca21ab1a247fcfe2b77a66c
EkaterinaKhmara <ekaterinakhmara01@gmail.com>
2020-07-23 21:21:06 +0300

    insert empty

--- index.php
+++ index.php
@@ -7,6 +7,11 @@ $html =  file_get_contents('https://muzoff.net/sborniki/lovemusic/');
 
 
 
+
+
+
+
+
 preg_match_all("/\<{1}a\s{1}class\={1}\"{1}play\-{1}dl\s{1}play\-{1}btn\"{1}\s{1}href\={1}\"{1}([a-zA-Z0-9\:\/\.\_\-]+)\"{1}/",$html,$array);
 
 
