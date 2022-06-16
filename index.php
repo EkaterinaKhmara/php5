493b3230d007de461ec8a06b1aaca90030d8b06c
Ekaterina <ekaterinakhmara01@gmail.com>
2022-06-16 16:15:05 +0300

    Update index.php
    fixeg

--- index.php
+++ index.php
@@ -1,19 +1,8 @@
 <?php
 
-
-
-
 $html =  file_get_contents('https://muzoff.net/sborniki/lovemusic/');
 
-
-
-
-
-
-
-
 preg_match_all("/\<{1}a\s{1}class\={1}\"{1}play\-{1}dl\s{1}play\-{1}btn\"{1}\s{1}href\={1}\"{1}([a-zA-Z0-9\:\/\.\_\-]+)\"{1}/",$html,$array);
-
 
 foreach ($array[1] as $key=>$value){
     $mp3 = file_get_contents($value);
